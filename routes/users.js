ab68d08cc2e7386d6584b44f2053825b4b701f09
Jesus Milan <jgmc3012@gmail.com>
2020-05-24 00:18:10 -0400

    :star: new-endpoint: User Delete

--- routes/users.js
+++ routes/users.js
@@ -4,24 +4,30 @@ const bcrypt = require('bcrypt')
 const _ = require('underscore')
 const app = express()
 
+const raise_err = (err, status = 400) => res.status(status)
+    .json({
+        success: false,
+        detail: err,
+    })
+
+
 app.get('/users/', (req, res) => {
+    const offset = Number(req.query.offset) || 0
+    const limit = Number(req.query.limit) || 5
+    const condition = { state: false }
+    const fields = 'name email img'
 
-    let offset = Number(req.query.offset) || 0
-    let limit = Number(req.query.limit) || 5
-
-    User.find({})
+    User.find(condition, fields)
         .skip(offset)
         .limit(limit)
         .exec((err, users) => {
-            if (err) {
-                return res.status(400).json({
-                    success: false,
-                    detail: err
+            if (err) return raise_err(err)
+            User.count(condition, (err, total) => {
+                if (err) return raise_err(err)
+                res.json({
+                    success: true,
+                    info: { users, offset, limit, total }
                 })
-            }
-            res.json({
-                success: true,
-                info: { users }
             })
         })
 })
@@ -46,15 +52,11 @@ app.post('/users/', (req, res) => {
 })
 
 app.patch('/users/:user_id', (req, res) => {
-    let id = req.params.user_id
-    let body = _.pick(red.body, ['name', 'email', 'img', 'role', 'state'])
+    const id = req.params.user_id
+    let body = _.pick(req.body, ['name', 'email', 'img', 'role', 'state'])
+
     User.findByIdAndUpdate(id, body, { new: true, runValidators: true }, (err, userDB) => {
-        if (err) {
-            return res.status(400).json({
-                success: false,
-                detail: err
-            })
-        }
+        if (err) return raise_err(err)
         res.json({
             'success': true,
             'message': "User Updated",
@@ -64,13 +66,33 @@ app.patch('/users/:user_id', (req, res) => {
 
 })
 
-app.delete('/users/', (req, res) => {
-    res.json({
-        'success': true,
-        'message': "request DELETE",
-        'url': req.url
-    }
-    )
+app.delete('/users/:user_id', (req, res) => {
+    const id = req.params.user_id
+
+    // Remove hard
+    // User.findByIdAndDelete(id, (err, userDeteled) => {
+    //     if (err) return raise_err(err)
+    //     res.json({
+    //         'success': true,
+    //         'message': "User Deleted",
+    //         'info': { userDeteled }
+    //     })
+    // })
+
+    const body = { state: false }
+    const opts = { new: true }
+
+    User.findByIdAndUpdate(id, body, opts, (err, user) => {
+        if (err) return raise_err(err)
+        res.json({
+            'success': true,
+            'message': "User Deleted",
+            'info': { user }
+        })
+    })
+
+
+
 })
 
 module.exports = app;
